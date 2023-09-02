eb542758fff6a7c8c367153ecb969fb852977e69
rowdyslav <rowdyslav@gmail.com>
2023-09-02 22:07:32 +0300

    Edit

--- Nim.py
+++ Nim.py
@@ -1,34 +1,42 @@
 import random
 
+
 def take_stones(player):
     if player == "Обезьянка":
-        hod_obez = STONES % (LIMIT + 1) if STONES % (LIMIT + 1) != 0 else random.randint(1, LIMIT)
+        hod_obez = (
+            STONES % (LIMIT + 1)
+            if STONES % (LIMIT + 1) != 0
+            else random.randint(1, LIMIT)
+        )
         STONES -= hod_obez
-        print(f'{player} взяла {hod_obez} камней, осталось {STONES}')
+        print(f"{player} взяла {hod_obez} камней, осталось {STONES}")
     else:
         hod_user = int(input())
         while hod_user > LIMIT or hod_user > STONES or hod_user <= 0:
-            print(f'Некорректный ход: {hod_user}')
+            print(f"Некорректный ход: {hod_user}")
             hod_user = int(input())
         STONES -= hod_user
-        print(f'Вы взяли {hod_user} камней, осталось {STONES}')
+        print(f"Вы взяли {hod_user} камней, осталось {STONES}")
     return STONES
+
 
 def main():
     global STONES, LIMIT
 
-    STONES = int(input('Сколько камней в куче? '))
-    LIMIT = int(input('Какой максимум камней можно взять за ход? '))
+    STONES = int(input("Сколько камней в куче? "))
+    LIMIT = int(input("Какой максимум камней можно взять за ход? "))
 
     while True:
         STONES = take_stones(STONES, LIMIT)
         if STONES == 0:
-            print('Обезьянка выиграла!')
-            break
-        
-        STONES = take_stones(STONES, LIMIT)
-        if STONES == 0:
-            print('Вы выиграли обезьянку!')
+            print("Обезьянка выиграла!")
             break
 
-main()
+        STONES = take_stones(STONES, LIMIT)
+        if STONES == 0:
+            print("Вы выиграли обезьянку!")
+            break
+
+
+if __name__ == "__main__":
+    main()
